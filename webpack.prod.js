0960097f098fc8b9a2b1ab192f0817f7c36c3880
丹治秀樹 <tanji@daitogiken.local>
2023-06-29 09:41:22 +0900

    i18nを動作するように修正。

--- webpack.prod.js
+++ webpack.prod.js
@@ -11,11 +11,11 @@ const path = require('path');
 
 /**@type {import('webpack').Configuration}*/
 const config = {
-    mode: 'development',
+    mode: 'production',
 
     target: 'node', // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
 
-    entry: './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+    entry: './out/src/extension.js', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
     output: { // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js',
@@ -24,6 +24,7 @@ const config = {
     },
     node: {
         __dirname: false,
+        //__filename: true,
     },
     devtool: 'source-map',
     externals: {
@@ -32,7 +33,7 @@ const config = {
     resolve: { // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
         extensions: ['.ts', '.js'],
         alias: {
-            "@cmt": path.resolve(__dirname, 'src')
+            "@cmt": path.resolve(__dirname, './out/src')
         },
         mainFields: ['main', 'module']
     },
