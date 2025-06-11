8f3c3cde324aac66da7b1e6069c4d387a8325820
丹治秀樹 <tanji@daitogiken.local>
2025-06-11 14:29:33 +0900

    デバッグターゲットの復元処理と拡張機能IDの修正
    - CMakeProject初期化時にデバッグターゲットを正しく復元する処理を追加
      - ワークスペース状態から起動ターゲット名を取得
      - デフォルト値が設定されていない場合に備えたフォールバック処理を実装
      - マルチプロジェクトフォルダ対応の状態保存処理を追加
    
    - 設定画面を開くコマンドの拡張機能IDを修正
      - '@ext:ms-vscode.cmake-tools' → '@ext:ms-vscode.cmake-tools-dg' に更新

--- src/extension.ts
+++ src/extension.ts
@@ -1880,7 +1880,7 @@ export class ExtensionManager implements vscode.Disposable {
     }
 
     openSettings() {
-        void vscode.commands.executeCommand('workbench.action.openSettings', '@ext:ms-vscode.cmake-tools');
+        void vscode.commands.executeCommand('workbench.action.openSettings', '@ext:ms-vscode.cmake-tools-dg');
     }
 
     async viewLog() {
